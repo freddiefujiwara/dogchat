85609d4aac3ee2143e49d0dc12bd70cdee5b3a3b
Fumikazu Fujiwara <github@ze.gs>
2020-10-13 13:03:35 +0900

    add dummy test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,9 +5,10 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:dogchat/main.dart';
-
-void main() {}
+void main() {
+  test('dummy', () {
+    expect(1, 1);
+  });
+}
