c0bcec17c97fde04fe40e25c5467e02c9faf0311
freddiefujiwara <github@ze.gs>
2020-10-13 09:40:37 +0000

    Deploying to gh-pages from  @ e0e59342e4d7b01224d67c69b398135b071fd9cd 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -11,8 +11,8 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "main.dart.js": "97d6085ce6d0c27c362257b9cae2d8ab",
 "manifest.json": "aca0242ba03c4d6fd20480fe8bd022c7",
-"index.html": "2a050bc75fd72aa7e27e5930cdb1cb99",
-"/": "2a050bc75fd72aa7e27e5930cdb1cb99",
+"index.html": "8bd8008361166b249327043a173a6477",
+"/": "8bd8008361166b249327043a173a6477",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1"
 };
