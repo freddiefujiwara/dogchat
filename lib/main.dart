659b5a29ed7baa72e91779d9c4be66ff602d9a77
Fumikazu Fujiwara <github@ze.gs>
2020-10-13 19:02:30 +0900

    add dart.js

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:google_sign_in/google_sign_in.dart';
 
 GoogleSignIn _googleSignIn = GoogleSignIn(
   scopes: <String>[
+    'email',
     'https://www.googleapis.com/auth/contacts.readonly',
   ],
 );
