191383987f062243a5d95f3017ef659a71a0d483
Fumikazu Fujiwara <github@ze.gs>
2020-10-15 17:07:06 +0900

    add column

--- lib/chat.dart
+++ lib/chat.dart
@@ -116,21 +116,30 @@ class MessageBubble extends StatelessWidget {
         crossAxisAlignment:
             isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
         children: [
-          Container(
-            width: 30,
-            height: 30,
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              image: DecorationImage(
-                  image: NetworkImage(this.Photo ??
-                      'https://freddiefujiwara.com/dogchat/favicon.png'),
-                  fit: BoxFit.fill),
-            ),
-          ),
-          Text(
-            "$Sender",
-            style: TextStyle(color: Colors.black87, fontSize: 12),
-          ),
+          Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Container(
+                        width: 30,
+                        height: 30,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          image: DecorationImage(
+                              image: NetworkImage(this.Photo ??
+                                  'https://freddiefujiwara.com/dogchat/favicon.png'),
+                              fit: BoxFit.fill),
+                        ),
+                      ),
+                      Text(
+                        "$Sender",
+                        style: TextStyle(color: Colors.black87, fontSize: 12),
+                      ),
+                    ]),
+              ]),
           Material(
             borderRadius: isMe ? Kborderradiusright : Kborderradiusleft,
             elevation: 10,
